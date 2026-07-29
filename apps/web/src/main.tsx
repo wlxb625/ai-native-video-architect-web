659b2f6f61c7fcdde358b98148179f8b1bead5c7
wlxb625 <133953421+wlxb625@users.noreply.github.com>
2026-07-29 13:52:15 +0800

    Load description composer styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './generation-v4.css';
+import './description-v5.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
